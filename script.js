b2ad2cda3173da6684a94e88c43a4e09b92a162a
zz <657835945@qq.com>
2023-09-05 22:44:42 +0800

    1

---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+ * @type {HTMLInputElement}
+ */
+let fileIn = document.querySelector('#in')
+
+fileIn.addEventListener('change', function (ev) {
+  console.log(fileIn.files)
+})
+
+fileIn.addEventListener('dragenter', function (ev) {
+  document.querySelector('.file-in').classList.add('active')
+})
+
+fileIn.addEventListener('dragleave', function (ev) {
+  document.querySelector('.file-in').classList.remove('active')
+})
+
+fileIn.addEventListener('drop', function (ev) {
+  document.querySelector('.file-in').classList.remove('active')
+})
