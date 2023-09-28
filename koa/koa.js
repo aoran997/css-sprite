a81a832772d60b906f67151d948bedbfdaedb380
mmmmm <mmmmm>
2023-09-28 17:20:51 +0800

    koa path

--- koa/koa.js
+++ koa/koa.js
@@ -1,7 +1,10 @@
 const server = require('koa-static')
 const Koa = require('koa');
+const path = require('path')
 const app = new Koa()
 
-app.use(server('../dist'))
+app.use(server(path.join(__dirname, 'dist')))
 
 app.listen(3000)
+
+console.log('server start:3000')
